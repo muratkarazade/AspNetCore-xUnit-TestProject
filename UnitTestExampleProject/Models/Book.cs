9be5da87375177c58d9d81c2688f44d163dc6470
Murat Kara <muratkarazade@gmail.com>
2023-08-07 00:05:40 +0300

    first commit

---
+++ UnitTestExampleProject/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExampleAPI.Models
+{
+    public class Book
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ UnitTestExampleProject/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using ExampleAPI.Models;
+
+namespace ExampleAPI.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book book);
+        Book GetById(Guid id);
+        Book Update(Book book);
+        void Delete(Guid id);
+    }
+}
